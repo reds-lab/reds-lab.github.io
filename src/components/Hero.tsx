0f0ec35e71e350cbf6b8e3e1d634ebdf23035899
Adam Nguyen <66979547+Adanato@users.noreply.github.com>
2024-06-22 12:41:35 -0500

    fixing image paths with direct imports

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,17 @@
 import EmblaCarousel from './EmblaCarousel';
 import { EmblaOptionsType } from 'embla-carousel';
 import '../styles/embla.css';
+import chrisCurry from '../assets/content/slides/chris-curry-GYpsSWHslHA-unsplash.jpg';
+import garyBendig from '../assets/content/slides/gary-bendig-6GMq7AGxNbE-unsplash.jpg';
+import rabbit from '../assets/content/slides/rabbit.jpeg';
+import rockPtarmigan from '../assets/content/slides/rock_ptarmigan.jpeg';
+import sandyMillar from '../assets/content/slides/sandy-millar-kKAaCeGf5wY-unsplash.jpg';
 
 const OPTIONS: EmblaOptionsType = { loop: true };
 
 // Hero.tsx
 function Hero() {
-  const imagePaths = [
-    '../assets/content/slides/chris-curry-GYpsSWHslHA-unsplash.jpg',
-    '../assets/content/slides/gary-bendig-6GMq7AGxNbE-unsplash.jpg',
-    '../assets/content/slides/rabbit.jpeg',
-    '../assets/content/slides/rock_ptarmigan.jpeg',
-    '../assets/content/slides/sandy-millar-kKAaCeGf5wY-unsplash.jpg',
-  ];
+    const imagePaths = [chrisCurry, garyBendig, rabbit, rockPtarmigan, sandyMillar];
 
   return (
     <section className="hero">
