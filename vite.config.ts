8d6d845b9f7d85a31fda19bda5ab886d81ab81fd
Adam Nguyen <66979547+Adanato@users.noreply.github.com>
2024-06-22 12:47:52 -0500

    fixing base routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/reds-lab.github.io/', // Replace with your repository name
+  base: '/', // Base URL for your project
   plugins: [react()],
 });
