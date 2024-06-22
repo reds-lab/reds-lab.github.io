413d0756845d38100fbf7aa2a80adf2da33434b1
Adam Nguyen <66979547+Adanato@users.noreply.github.com>
2024-06-22 13:05:32 -0500

    fixing base route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom'
+// main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-    <App />
+      <App />
     </HashRouter>
   </React.StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Base URL for your project
   plugins: [react()],
 });
