67b867e9f3f536b61b68a23098f5ec50d29af66e
Adam Nguyen <66979547+Adanato@users.noreply.github.com>
2024-06-27 09:52:22 -0500

    Improved People section with new profile photos and css

--- src/assets/content/People/People.ts
+++ src/assets/content/People/People.ts
@@ -3,9 +3,9 @@ export const peopleData = [
     title: "Faculty Members",
     people: [
       {
-        profileImage: "src/assets/content/people/profile/ruoxi-jia.jpg",
+        profileImage: "src/assets/content/people/profile/ruoxi jia.png",
         name: "Ruoxi Jia",
-        details: "Ruoxi Jia is an Assistant Professor in the Computer Science Department. Her work is focused on the design of algorithms for AI agents and autonomous systems that safely and reliably interact with other intelligent agents including humans.",
+        details: "Ruoxi Jia is an Assistant Professor in the Electrical and Computer Engineering Department. Her work is focused on the design of algorithms for AI agents and autonomous systems that safely and reliably interact with other intelligent agents including humans.",
         links: {
           website: "https://ruoxijia.com",
           github: "https://github.com/ruoxijia",
@@ -18,28 +18,15 @@ export const peopleData = [
   },
   {
     title: "Postdocs and Visiting Scholars",
-    people: [
-      {
-        profileImage: "src/assets/content/people/profile/postdoc.jpg",
-        name: "Postdoc Name",
-        details: "Postdoc is a postdoc in the Computer Science Department. Their research focuses on efficient algorithms for enabling low-effort human teaching of robots.",
-        links: {
-          website: "https://postdoc.com",
-          github: "https://github.com/postdoc",
-          googleScholar: "https://scholar.google.com/citations?user=def456",
-          linkedin: "https://www.linkedin.com/in/postdoc",
-          twitter: "https://twitter.com/postdoc"
-        }
-      }
-    ]
+    people: []
   },
   {
     title: "PhD Students",
     people: [
       {
-        profileImage: "src/assets/content/people/profile/yi-zeng.jpg",
+        profileImage: "src/assets/content/people/profile/yi zeng.png",
         name: "Yi Zeng",
-        details: "Yi is a PhD student in the Computer Science department. He is broadly interested in enabling robots to perceive and reason about dynamic real world environments.",
+        details: "Yi is a PhD student in the Electrical and Computer Engineering department. He is broadly interested in enabling robots to perceive and reason about dynamic real world environments.",
         links: {
           website: "https://yizeng.com",
           github: "https://github.com/yizeng",
@@ -49,40 +36,9 @@ export const peopleData = [
         }
       },
       {
-        profileImage: "src/assets/content/people/profile/myeonseob-ko.jpg",
-        name: "Myeonseob Ko",
-        details: "Myeonseob is a PhD student in the Computer Science Department. His work is focused on the intersection of human-robot-interaction and natural language processing.",
-        coAdvised: "(Co-advised with Percy Liang)",
-        links: {
-          website: "https://myeonseob.com",
-          github: "https://github.com/myeonseob",
-          googleScholar: "https://scholar.google.com/citations?user=ghi789",
-          linkedin: "https://www.linkedin.com/in/myeonseob-ko",
-          twitter: "https://twitter.com/myeonseob"
-        }
-      },
-     
-      {
-        profileImage: "src/assets/content/people/profile/hoang-just.jpg",
-        name: "Hoang Just",
-        details: "Hoang is a PhD student in the Computer Science department. His research focuses on developing robust and efficient algorithms for autonomous systems.",
-        links: {
-          website: "https://hoangjust.com",
-          github: "https://github.com/hoangjust",
-          googleScholar: "https://scholar.google.com/citations?user=mno345",
-          linkedin: "https://www.linkedin.com/in/hoang-just",
-          twitter: "https://twitter.com/hoangjust"
-        }
-      }
-    ]
-  },
-  {
-    title: "MS Students",
-    people: [
-      {
-        profileImage: "src/assets/content/people/profile/si-chen.jpg",
+        profileImage: "src/assets/content/people/profile/si chen.png",
         name: "Si Chen",
-        details: "Si is an MS student in the Computer Science department. Her research interests include machine learning and computer vision.",
+        details: "Si is a PhD student in the Electrical and Computer Engineering department. Her research interests include machine learning and computer vision.",
         links: {
           website: "https://sichen.com",
           github: "https://github.com/sichen",
@@ -92,15 +48,57 @@ export const peopleData = [
         }
       },
       {
-        profileImage: "src/assets/content/people/profile/mahivir-dabas.jpg",
-        name: "Mahivir Dabas",
-        details: "Mahivir is an MS student in the Computer Science department. His work focuses on developing intelligent systems for robotics applications.",
+        profileImage: "src/assets/content/people/profile/myeonseob ko.png",
+        name: "Myeonseob Ko",
+        details: "Myeonseob is a PhD student in the Electrical and Computer Engineering Department. His work is focused on the intersection of human-robot-interaction and natural language processing.",
+        coAdvised: "",
         links: {
-          website: "https://mahivirdabas.com",
-          github: "https://github.com/mahivirdabas",
+          website: "https://myeonseob.com",
+          github: "https://github.com/myeonseob",
+          googleScholar: "https://scholar.google.com/citations?user=ghi789",
+          linkedin: "https://www.linkedin.com/in/myeonseob-ko",
+          twitter: "https://twitter.com/myeonseob"
+        }
+      },
+      {
+        profileImage: "src/assets/content/people/profile/hoang just.png",
+        name: "Hoang Just",
+        details: "Hoang is a PhD student in the Electrical and Computer Engineering department. His research focuses on developing robust and efficient algorithms for autonomous systems.",
+        links: {
+          website: "https://hoangjust.com",
+          github: "https://github.com/hoangjust",
+          googleScholar: "https://scholar.google.com/citations?user=mno345",
+          linkedin: "https://www.linkedin.com/in/hoang-just",
+          twitter: "https://twitter.com/hoangjust"
+        }
+      },
+      {
+        profileImage: "src/assets/content/people/profile/feiyang kang.png",
+        name: "Feiyang Kang",
+        details: "Feiyang is a PhD student in the Electrical and Computer Engineering department. His research focuses on advanced machine learning techniques for robotics and autonomous systems.",
+        links: {
+          website: "https://feiyangkang.com",
+          github: "https://github.com/feiyangkang",
+          googleScholar: "https://scholar.google.com/citations?user=fei789",
+          linkedin: "https://www.linkedin.com/in/feiyang-kang",
+          twitter: "https://twitter.com/feiyangkang"
+        }
+      }
+    ]
+  },
+  {
+    title: "MS Students",
+    people: [
+      {
+        profileImage: "src/assets/content/people/profile/mahavir dabas.png",
+        name: "Mahavir Dabas",
+        details: "Mahavir is an MS student in the Electrical and Computer Engineering department. His work focuses on developing intelligent systems for robotics applications.",
+        links: {
+          website: "https://mahavirdabas.com",
+          github: "https://github.com/mahavirdabas",
           googleScholar: "https://scholar.google.com/citations?user=stu901",
-          linkedin: "https://www.linkedin.com/in/mahivir-dabas",
-          twitter: "https://twitter.com/mahivirdabas"
+          linkedin: "https://www.linkedin.com/in/mahavir-dabas",
+          twitter: "https://twitter.com/mahavirdabas"
         }
       }
     ]
@@ -109,7 +107,7 @@ export const peopleData = [
     title: "Undergraduate Students",
     people: [
       {
-        profileImage: "src/assets/content/people/profile/adam-nguyen.jpg",
+        profileImage: "src/assets/content/people/profile/adam nguyen.png",
         name: "Adam Nguyen",
         details: "Adam is an undergraduate student in the Computer Science department. He is interested in exploring the applications of AI in real-world scenarios.",
         links: {
@@ -126,27 +124,15 @@ export const peopleData = [
     title: "Past Alumni",
     people: [
       {
-        profileImage: "src/assets/content/people/profile/alumni1.jpg",
-        name: "Alumni Name 1",
-        details: "Alumni 1 was a PhD student in the Computer Science department. They worked on developing advanced algorithms for autonomous systems.",
+        profileImage: "src/assets/content/people/profile/minzhou pan.png",
+        name: "Minzhou Pan",
+        details: "Minzhou was a PhD student in the Electrical and Computer Engineering department. They worked on developing advanced algorithms for autonomous systems.",
         links: {
-          website: "https://alumni1.com",
-          github: "https://github.com/alumni1",
-          googleScholar: "https://scholar.google.com/citations?user=yza567",
-          linkedin: "https://www.linkedin.com/in/alumni1",
-          twitter: "https://twitter.com/alumni1"
-        }
-      },
-      {
-        profileImage: "src/assets/content/people/profile/alumni2.jpg",
-        name: "Alumni Name 2",
-        details: "Alumni 2 was an MS student in the Computer Science department. Their research focused on applying machine learning techniques to robotics.",
-        links: {
-          website: "https://alumni2.com",
-          github: "https://github.com/alumni2",
-          googleScholar: "https://scholar.google.com/citations?user=bcd890",
-          linkedin: "https://www.linkedin.com/in/alumni2",
-          twitter: "https://twitter.com/alumni2"
+          website: "https://minzhoupan.com",
+          github: "https://github.com/minzhoupan",
+          googleScholar: "https://scholar.google.com/citations?user=min567",
+          linkedin: "https://www.linkedin.com/in/minzhou-pan",
+          twitter: "https://twitter.com/minzhoupan"
         }
       }
     ]
