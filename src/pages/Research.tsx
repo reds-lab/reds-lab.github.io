d4fe691762b5c6e9e893d839b14d55e7c7ce3e10
Adam Nguyen <66979547+Adanato@users.noreply.github.com>
2024-07-12 14:42:36 -0500

    trying to debug gifs

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -7,6 +7,10 @@ import ssaImage from  "../assets/content/Research/safetysecurityalignment.gif"
 import privacyImage from "../assets/content/Research/privacy.webp"
 
 const Research: React.FC = () => {
+  console.log('Data-Centric Image Path:', dataCentric);
+  console.log('Safety Security Alignment Image Path:', ssaImage);
+  console.log('Privacy Image Path:', privacyImage);
+
   return (
     <div className="research-page">
       <header className="research-header">
@@ -22,30 +26,32 @@ const Research: React.FC = () => {
         </p>
 
         <table className="research-bubble">
-          <tr>
-            <td className="rb-img">
-              <img src={dataCentric} alt="Data-Centric AI" />
-            </td>
-            <td className="rb-text">
-              <Link to="/research/data-centric-ai">Better Data, Better Models</Link>
-            </td>
-          </tr>
-          <tr>
-            <td className="rb-img">
-               <img src={ssaImage} alt="Safety, Security, and Alignment" />
-            </td>
-            <td className="rb-text">
-              <Link to="/research/safety-security-alignment">Safety, Security, and Alignment</Link>
-            </td>
-          </tr>
-          <tr>
-            <td className="rb-img">
-              <img src={privacyImage} alt="Privacy" />
-            </td>
-            <td className="rb-text">
-              <Link to="/research/privacy">Privacy Risk Assessment</Link>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="rb-img">
+                <img src={dataCentric} alt="Data-Centric AI" />
+              </td>
+              <td className="rb-text">
+                <Link to="/research/data-centric-ai">Better Data, Better Models</Link>
+              </td>
+            </tr>
+            <tr>
+              <td className="rb-img">
+                 <img src={ssaImage} alt="Safety, Security, and Alignment" />
+              </td>
+              <td className="rb-text">
+                <Link to="/research/safety-security-alignment">Safety, Security, and Alignment</Link>
+              </td>
+            </tr>
+            <tr>
+              <td className="rb-img">
+                <img src={privacyImage} alt="Privacy" />
+              </td>
+              <td className="rb-text">
+                <Link to="/research/privacy">Privacy Risk Assessment</Link>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </article>
     </div>
